bc4e3ef14cc7ce907dc8a824a7b72d603660f03c
Ayman Jaaouani <ayman.jaaouani.aj@gmail.com>
2023-11-05 15:10:30 +0200

    rubocop fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
+  def logo(style = "h-24 w-auto")
+    image_tag "schedule_master_logo.png", class: style, alt: "Schedulemaster"
+  end
+
+  def logo_with_link(style, link = root_path)
+    link_to link do
+      logo(style)
+    end
+  end
 end
--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -1,11 +1,12 @@
-require 'rails_helper'
+# frozen_string_literal: true
 
-RSpec.describe "Homes", type: :request do
+require "rails_helper"
+
+RSpec.describe "Homes" do
   describe "GET /index" do
     it "returns http success" do
       get "/home/index"
       expect(response).to have_http_status(:success)
     end
   end
-
 end
